02455bebda57691c78cf2ca38f3ad691586de907
Damien-trqr <turquier.damien@gmail.com>
2018-10-11 19:21:05 +0200

    PSR2 standard correct

--- src/Model/PlayerManager.php
+++ src/Model/PlayerManager.php
@@ -39,11 +39,10 @@ class PlayerManager extends AbstractManager
 
     public function delete(int $id): void
     {
-        if($_SERVER['REQUEST_METHOD'] === 'GET') {
-        $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
+        if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+            $statement = $this->pdo->prepare("DELETE FROM $this->table WHERE id=:id");
+            $statement->bindValue('id', $id, \PDO::PARAM_INT);
+            $statement->execute();
         }
     }
 }
-
