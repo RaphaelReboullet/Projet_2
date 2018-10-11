9e27f608de1518585fe7e3ed4c40eacba0c7f7bb
Damien-trqr <turquier.damien@gmail.com>
2018-10-11 19:09:52 +0200

    fonctionnalité delPlayer rdy

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -43,8 +43,15 @@ class PlayerController extends AbstractController
             $player->setNumber($_POST['number']);
             $player->setIsactif($_POST['isactif']);
             $id = $playerManager->insert($player);
-            header('Location:/player/' . $id);
+            header('Location:team/player/' . $id);
         }
         return $this->twig->render('Player/add.html.twig');
+    }
+
+    public function delPlayer(int $id)
+    {
+        $playerManager = new PlayerManager($this->getPdo());
+        $playerManager->delete($id);
+        header('Location:/newteam');
     }
 }
